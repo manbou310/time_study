c4aeccfda17b549b722810dc824d5729d24acd86
manbou310 <hare.sunshine09@gmail.com>
2021-07-20 23:56:00 +0900

    詳細画面実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users , only:[:edit, :update]
   root to: "channels#index"
-  resources :channels, only: [:new, :create, :destroy] do
-    resources :works, only:[:index, :new, :create]
+  resources :channels, only: [:new, :create, :destroy] ,shallow: true do
+    resources :works, only:[:index, :new, :create, :show]
     
   end
 end
